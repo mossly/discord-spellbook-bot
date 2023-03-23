80536319b8a5c7837053257827a04c3958a6ec16
Mossly <moss.aaron@icloud.com>
2023-03-23 17:23:44 +1300

    try checking message reference to determine reply

--- discordbot.py
+++ discordbot.py
@@ -39,23 +39,25 @@ async def on_message(message):
     if message.author == bot.user:
         return
 
-    if message.content.find('@MS-DOS-LY#1956') != -1:
-
-        response = await send_request(message.content)
-        
-        if response.status_code == 200:
-            await message.channel.send(f'Hi {message.author.mention} \n'+ response.json()['output'].strip())
-        else:
-            await message.channel.send(f'Sorry {message.author.mention}, something went wrong.')    
-        
     if message.reference and message.reference.resolved.author == bot.user:
     
-        response = await send_request(message.content)
+        if message.reference and message.reference.cached_message is not None:
         
-        if response.status_code == 200:
-            await message.channel.send(response.json()['output'].strip())
+            response = await send_request(message.content)
+            
+            if response.status_code == 200:
+                await message.channel.send(f'Hi {message.author.mention} \n'+ response.json()['output'].strip())
+            else:
+                await message.channel.send(f'Sorry {message.author.mention}, something went wrong.')    
+                
         else:
-            await message.channel.send(f'Sorry {message.author.mention}, something went wrong.') 
+    
+            response = await send_request(message.content)
+            
+            if response.status_code == 200:
+                await message.channel.send(response.json()['output'].strip())
+            else:
+                await message.channel.send(f'Sorry {message.author.mention}, something went wrong.') 
 
 TOKEN = os.getenv("BOT_API_TOKEN")
 
